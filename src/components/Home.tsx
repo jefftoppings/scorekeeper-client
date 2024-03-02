c497eb8ae0e2eaf3ab8e4ed3c7f15363fd1a8c07
Jeff Toppings <jeffrey.toppings@outlook.com>
2024-03-02 10:39:13 -0700

    feat: WIP wiring up Home.tsx

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,18 +1,36 @@
-import { Box, Container, Heading } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Container,
+  Heading,
+  Select,
+  Stack,
+} from "@chakra-ui/react";
 import React from "react";
-import { Link } from "react-router-dom";
 
 const Home: React.FC = () => {
   return (
     <Container>
-      <Box bg="tomato" w="100%" p={4} color="white">
-        This is the Box
-        <Heading>Hello Chakra UI!</Heading>
-        <div>
-          <h1>Home Page</h1>
-          <p>Welcome to the home page!</p>
-          <Link to="/new-game">Go to New Game</Link>
-        </div>
+      <Box
+        w="100%"
+        p={4}
+        color="white"
+        display="flex"
+        alignItems="center"
+        flexDir="column"
+      >
+        <Heading>Scorekeeper</Heading>
+        <Select placeholder="Choose game" margin="24px 0 0">
+          <option value="mexican-train-game">Mexican Train Game</option>
+        </Select>
+        <Stack direction="row" spacing={4} align="center" margin="16px 0">
+          <Button colorScheme="teal" variant="solid">
+            New Game
+          </Button>
+          <Button colorScheme="teal" variant="solid">
+            Continue
+          </Button>
+        </Stack>
       </Box>
     </Container>
   );
